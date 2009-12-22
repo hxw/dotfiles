a18342f7751c11fadd0417f6808744383cb8532b
Christopher Hall <hsw@openmoko.com>
2009-12-22 09:50:57 +0800

    Initial creation of dotfiles repository
    Signed-off-by: Christopher Hall <hsw@openmoko.com>

---
+++ bash_aliases
@@ -0,0 +1,53 @@
+# .bash_aliases
+
+alias ll='ls -l'
+alias la='ls -A'
+alias lc='ls -CF'
+
+# gnu diff does not have DIFFOPTIONS like FreeBSD so:
+alias diff='diff -u'
+
+alias reb='find . -name \*~ -print -delete'
+
+export LESS="-iR"
+
+if which eie > /dev/null 2>&1
+then
+  alias edit="eie --no-frame"
+  alias ed="eie --no-wait"
+  export EDITOR="eie"
+elif which jove > /dev/null 2>&1
+then
+  alias edit="jove"
+  alias ed="jove"
+  export EDITOR="jove"
+fi
+
+# give lynx a custom configuration
+alias lynx='lynx -nopause'
+
+mkcd()
+{
+  local dir
+  dir="$1"; shift
+
+  if [ -z "${dir}" ]
+  then
+    pwd
+  elif [ -d "${dir}" ]
+  then
+    cd "${dir}"
+  elif [ -f "${dir}" ]
+  then
+   echo A file of that name already exists
+   return 1
+  else
+    mkdir -p "${dir}"
+    cd "${dir}"
+  fi
+  return 0
+}
+
+if [ -f ~/.bash_aliases_extra ]; then
+    . ~/.bash_aliases_extra
+fi
---
+++ install.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+list_sed=''
+list_copy=''
+
+SED()
+{
+  list_sed="${list_sed} $*"
+}
+
+COPY()
+{
+  list_copy="${list_copy} $*"
+}
+
+# list the files to install
+# SED  substitutes @NAME@ type of entries
+# COPY just copies the file
+
+SED gitconfig
+
+COPY bashrc
+COPY joverc
+COPY tcshrc
+
+
+# end of list
+# ===========
+
+ERROR()
+{
+  echo error: $* 1>&2
+  exit 1
+}
+
+# get a string escaping / & for later sed substitution
+# usage:  var=$(get prompt message here) || exit 1
+# note: need the exit as the ERROR only terminates the $() sub-shell
+get()
+{
+  local prompt data
+  read -p "$*: " -r data
+  [ -z "${data}" ] && ERROR field cannot be blank
+  printf '%s' "${data}" | sed 's/\\/\\\\/g;s,/,\\/,g;s,&,\\\&,g'
+}
+
+
+# main
+# ====
+
+echo this will install the files to: ${HOME}
+echo Ctrl-C to abort
+
+
+echo Enter some data for substitutions
+
+name=$(get Enter full name) || exit 1
+email=$(get Enter email address) || exit 1
+
+for f in ${list_sed}
+do
+  d="${HOME}/.${f}"
+  echo Substitute ${f} to ${d}
+  sed "s,@HOME@,${HOME}/,g;s/@EMAIL@/${email}/g;s/@NAME@/${name}/g;" "${f}" Z "${d}"
+done
+
+for f in ${list_copy}
+do
+  d="${HOME}/.${f}"
+  echo Copy ${f} to ${d}
+  cp -p "${f}" "${d}"
+done
