7cb47ac21ab33025bd6e291305a5e85d971c0266
Christopher Hall <hsw@ms2.hinet.net>
2019-12-17 21:31:51 +0800

    [xmonad] sync configs
    Signed-off-by: Christopher Hall <hsw@ms2.hinet.net>

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -22,7 +22,8 @@ main = do
        , modMask = mod4Mask     -- Rebind Mod to the Windows key
        } `additionalKeys`
        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
-       , ((mod4Mask, xK_b), sendMessage ToggleStruts)
-       , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-       , ((0, xK_Print), spawn "scrot")
+       , ((mod4Mask, xK_Pause), spawn "xscreensaver-command -lock; xset dpms force off")
+       , ((mod4Mask, xK_b), sendMessage ToggleStruts)            -- toggle xmobar
+       -- , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+       -- , ((0, xK_Print), spawn "scrot")
        ]
