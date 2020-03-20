b314ef39d8ae836739cc435d78242549e1719b55
Christopher Hall <hsw@ms2.hinet.net>
2020-03-20 17:26:01 +0800

    [xmonad] change layout order
    starts with full and adds a couple of others
    
    Signed-off-by: Christopher Hall <hsw@ms2.hinet.net>

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -4,16 +4,36 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Layout.Circle
+import XMonad.Layout.Spiral
 import XMonad.Actions.WindowGo
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import Data.Ratio
+
+
+myLayoutHook = avoidStruts (Full ||| tiled ||| Mirror tiled ||| spiral (1 % 1) ||| Circle)
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled = Tall nmaster delta ratio
+
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     ratio = 2/3
+
+     -- Percent of screen to increment by when resizing panes
+     delta = 3/100
+
 
 main = do
     xmproc <- spawnPipe "xmobar"
 
     xmonad $ defaultConfig
        { manageHook = manageDocks <+> manageHook defaultConfig
-       , layoutHook = avoidStruts  $  layoutHook defaultConfig
+       --, layoutHook = avoidStruts  $  layoutHook defaultConfig
+       , layoutHook = myLayoutHook
        , handleEventHook    = handleEventHook defaultConfig <+> docksEventHook
        , logHook = dynamicLogWithPP xmobarPP
                    { ppOutput = hPutStrLn xmproc
