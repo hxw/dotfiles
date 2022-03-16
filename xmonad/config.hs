275946a036edd62633f9948479c14c0499f5bff6
Christopher Hall <hsw@ms2.hinet.net>
2022-03-16 15:52:49 +0800

    [xmonad] Firefox class name lower cased in firefox-98.0_2,2
    Signed-off-by: Christopher Hall <hsw@ms2.hinet.net>

--- xmonad/config.hs
+++ xmonad/config.hs
@@ -75,7 +75,7 @@ main = do
        -- , ((0, xK_Print), spawn "scrot")
        , ((mod4Mask, xK_c), runOrRaise "conlecterm" (className =? "Conlecterm"))
        , ((mod4Mask, xK_e), runOrRaise "emacs" (className =? "Emacs"))
-       , ((mod4Mask, xK_f), runOrRaise "firefox" (className =? "Firefox"))
+       , ((mod4Mask, xK_f), runOrRaise "firefox" (className =? "firefox"))
        , ((mod4Mask, xK_h), runOrRaise "hexchat" (className =? "Hexchat"))
        , ((mod4Mask, xK_p), spawn "dmenu_run -p 'run>' -fn '-Fixed-Bold-R-Normal-*-16-*-*-*-*-*-*-*' -sb grey25 -sf hotpink -nb blue -nf white")
        , ((mod4Mask, xK_u), runOrRaise "urxvt" (className =? "URxvt"))
